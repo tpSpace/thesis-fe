07762ef68e3459cef721b26b24a285033f06af04
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2025-02-25 22:32:11 +0700

    Save

--- src/utils/apollo-client.js
+++ src/utils/apollo-client.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import {setContext} from "@apollo/client/link/context";
-import {ApolloClient, ApolloLink, createHttpLink, InMemoryCache} from "@apollo/client";
-import {onError} from "@apollo/client/link/error";
-import {isValidToken} from "./jwt";
+import React from "react";
+import { setContext } from "@apollo/client/link/context";
+import {
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { isValidToken } from "./jwt";
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors) {
-        console.log('graphQLErrors: ', graphQLErrors);
-    }
-    if (networkError) {
-        console.log('networkError: ', networkError);
-    }
+  if (graphQLErrors) {
+    console.log("graphQLErrors: ", graphQLErrors);
+  }
+  if (networkError) {
+    console.log("networkError: ", networkError);
+  }
 });
 
 const httpLink = createHttpLink({
-    uri: process.env.NODE_ENV === 'development' ? "http://localhost:4000/graphql" : "https://api.spacex.land/graphql",
-    credentials: 'include'
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/graphql"
+      : "http://localhost:4000/graphql",
+  credentials: "include",
 });
 
 const authLink = setContext((_, { headers }) => {
-    const accessToken = window.localStorage.getItem('accessToken');
-    return {
-        headers: {
-            ...headers,
-            authorization: isValidToken(accessToken) ? `Bearer ${accessToken}` : ''
-        }
-    };
+  const accessToken = window.localStorage.getItem("accessToken");
+  return {
+    headers: {
+      ...headers,
+      authorization: isValidToken(accessToken) ? `Bearer ${accessToken}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(ApolloLink.from([errorLink, httpLink])),
-    cache: new InMemoryCache()
+  link: authLink.concat(ApolloLink.from([errorLink, httpLink])),
+  cache: new InMemoryCache(),
 });
 
 export default client;
