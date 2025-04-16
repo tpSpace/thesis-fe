8c8eae72759b807c5bc651af2f1f9a81f4a688c0
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2025-04-16 11:41:43 +0000

    add new libs & and workaround for gettting backend ip

--- src/utils/apollo-client.js
+++ src/utils/apollo-client.js
@@ -9,23 +9,18 @@ import { onError } from "@apollo/client/link/error";
 import { isValidToken } from "./jwt";
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    console.log("graphQLErrors: ", graphQLErrors);
-  }
-  if (networkError) {
-    console.log("networkError: ", networkError);
-  }
+  if (graphQLErrors) console.log("graphQLErrors: ", graphQLErrors);
+  if (networkError) console.log("networkError: ", networkError);
 });
 
+// Placeholder URI; will be overridden
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI + "/graphql",
+  uri: "http://placeholder:4000/graphql",
   credentials: "include",
 });
-console.log("httpLink: ", httpLink);
-console.log(process.env.NEXT_PUBLIC_GRAPHQL_URI);
-console.log(process.env.BACKEND_PORT);
+
 const authLink = setContext((_, { headers }) => {
-  if (typeof window === "undefined") return { headers }; // SSR guard
+  if (typeof window === "undefined") return { headers };
   const accessToken = window.localStorage.getItem("accessToken");
   return {
     headers: {
@@ -35,14 +30,33 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const client = new ApolloClient({
-  link: ApolloLink.from([errorLink, authLink, httpLink]),
-  cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: "cache-and-network",
-    },
-  },
-});
+// Function to create Apollo Client with dynamic URI
+export async function createApolloClient() {
+  let backendUrl =
+    process.env.NEXT_PUBLIC_GRAPHQL_URI || "http://placeholder:4000";
+  try {
+    const res = await fetch("/api/backend-url");
+    const { backendUrl: fetchedUrl } = await res.json();
+    if (fetchedUrl) backendUrl = fetchedUrl;
+  } catch (err) {
+    console.error("Failed to fetch backend URL:", err);
+  }
 
-export default client;
+  const dynamicHttpLink = createHttpLink({
+    uri: `${backendUrl}/graphql`,
+    credentials: "include",
+  });
+
+  return new ApolloClient({
+    link: ApolloLink.from([errorLink, authLink, dynamicHttpLink]),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: "cache-and-network",
+      },
+    },
+  });
+}
+
+// Export a promise-based client for use in components
+export default createApolloClient();
