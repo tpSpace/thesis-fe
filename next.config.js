f4fa901859263a0b2af24f7a544988c33097bcd4
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2025-06-13 00:10:04 +0700

    change default path to /app

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-// filepath: /home/khoi/Documents/IU/thesis/lcasystem-FE/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  // Configure base path for /app routing
+  basePath: '/app',
+  // Asset prefix for static files
+  assetPrefix: '/app',
   // ... other configurations
   publicRuntimeConfig: {
     NEXT_PUBLIC_GRAPHQL_URI: process.env.NEXT_PUBLIC_GRAPHQL_URI,
