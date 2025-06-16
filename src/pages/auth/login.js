b0400d57579621372c0cec637bf1c6717a487ba1
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2025-06-16 11:16:58 +0700

    test ci/cd

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -79,7 +79,7 @@ export default function Login() {
           {mdUp && (
             <SectionStyle>
               <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-                Hi, Welcome Back Sir
+                Hi, Welcome Back
               </Typography>
             </SectionStyle>
           )}
